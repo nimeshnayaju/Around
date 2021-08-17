fc67f2f903557963649c4bcca05af41786d7b323
nimeshnayaju <nimeshnayaju@gmail.com>
2021-08-17 15:06:58 -0700

    Initial Commit

---
+++ Around/AroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundApp.swift
+//  Around
+//
+//  Created by Nimesh Nayaju on 2021-08-17.
+//
+
+import SwiftUI
+
+@main
+struct AroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
